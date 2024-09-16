1637485b59d0faf354a0be35c1b86676c025cbf6
Delvin <70066996+delvin220@users.noreply.github.com>
2024-09-16 11:19:55 -0400

    Add files via upload

---
+++ H4X0R News/H4X0R News/H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NewsApp.swift
+//  H4X0R News
+//
+//  Created by delvin han on 2024-08-30.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
